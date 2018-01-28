43f22edb02ea7225fe1cef4fde92764059c66cff
Merlin Call <mercall@uat.edu>
2018-01-28 04:53:19 -0800

    Version 2.0 were the guessing along with the number generator is compouter controlled. The problem isw that thoguh the guess is random it does not calculate the middle and guess that one and then loops back depending on whether the guess was higher or lower

--- Merlin Call Skynet HK-Ariel/Merlin Call Skynet HK-Ariel.cpp
+++ Merlin Call Skynet HK-Ariel/Merlin Call Skynet HK-Ariel.cpp
@@ -1,17 +1,19 @@
-// Skynet HK-Ariel.cpp : Defines the entry point for the console application.
-
+// Merlin Call Skynet HK.cpp : Defines the entry point for the console application.
 //Guess My number
 //THe classic number guessing game
 
 //line told to add/starting included line
 #include "stdafx.h"
 
+//includes iostream, cstdlib, and ctime tio the program code
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
+//lets the code know that we are using the namespace std
 using namespace std;
 
+//main role of the program
 int main()
 {
 	//seed random number generator
@@ -23,33 +25,60 @@ int main()
 	int tries = 0;
 	int guess;
 
-	cout << "\tWelcome to Guess My Number\n\n";
+	//integers naming the highest and lowest guess, the range of numbers to be guesses in adn the number to be offset by at least
+	int highestguess = 64;
+	int lowestguess = 0;
+	int range = 64;
+	int offset = 1;
 
+	//integer named computer number
+	int computerNumber;
+
+	//tyoes out the opening line
+	cout << "\tWelcome to Skynet HK-Ariel\n\n";
+
+	//does depending on what the cope prints out
 	do
 	{
-		cout << "Enter a guess: ";
-		cin >> guess;
-		++tries;
+		//random number between the integers of range and offset
+		computerNumber = rand() % range + offset; 
 
-		if (guess > secretNumber)
+		//does something if the computerNumber > secretNumber
+		if (computerNumber > secretNumber)
 		{
-			cout << "Too high!\n\n";
+			//prints out whether the guess is to low and the number it guessed and whether the targets where there or not
+			//then resets the ranges to reflect based on the previous guess whether it was higher or lower
+			cout << "The Skynet Hk has not found it's target! (too high)\n";
+			cout << "Grid square searched " << computerNumber << "\n\n";
+			highestguess = computerNumber;
+			range = highestguess - lowestguess - 1;
+			++tries;
 		}
-		
-		else if (guess < secretNumber)
+		//does something if the computerNumber < secretNumber
+		else if (computerNumber < secretNumber)
 		{
-			cout << "Too low!\n\n";
+			//prints out whether the guess is to low and the number it guessed and whether the targets where there or not
+			//then resets the ranges to reflect based on the previous guess whether it was higher or lower
+			cout << "The Skynet Hk has not found it's target! (too low)\n";
+			cout << "Grid square searched " << computerNumber << "\n\n";
+			lowestguess = computerNumber;
+			range = highestguess - lowestguess - 1;
+			offset = lowestguess + 1;
+			++tries;
 		}
 
+		//does something if not one of the two previous statements
 		else
 		{
-			cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+			//prints out that the computers guess was right and what the number was and how many tries it wook
+			cout << "The has found it's target! They are in grid space " << secretNumber << " and took " << tries << " tries\n";
 		}
-	} while (guess != secretNumber);
+		//this states that while computerNumber != secretNumber
+	} while (computerNumber != secretNumber);
 
 	//this will pause the system so you can see the printed info
 	system("pause");
-
+	
+	//thi causes the execution to leave the current subrutine
 	return 0;
 }
-
